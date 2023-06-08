50cebfa0317d5317e871950e6e52bcfd4771b42a
우태진 <taejin.woo@vtw.co.kr>
2023-06-08 10:20:42 +0900

    Merge

--- packages/angular/src/app/components/utils/form-popup/form-popup.component.ts
+++ packages/angular/src/app/components/utils/form-popup/form-popup.component.ts
@@ -40,6 +40,10 @@ export class FormPopupComponent {
 
   constructor(protected screen: ScreenService) { }
 
+  onCancelClick() {
+    this.close();
+  }
+
   isValid() {
     return this.validationGroup.instance.validate().isValid;
   }
