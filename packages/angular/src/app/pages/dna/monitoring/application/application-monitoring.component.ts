603935e13e7a9ddaaa226b727433259ab7c982cf
임병렬 <byeongryeol.im@vtw.co.kr>
2023-07-19 18:23:12 +0900

    Application Monitoring 서버별 조회

--- packages/angular/src/app/pages/dna/monitoring/application/application-monitoring.component.ts
+++ packages/angular/src/app/pages/dna/monitoring/application/application-monitoring.component.ts
@@ -1,7 +1,9 @@
 import {Component, NgModule} from "@angular/core";
 import {SafePipeModule} from "../../../../pipes/SafePipe";
-import {DxSelectBoxModule} from "devextreme-angular";
-import {Apollo} from "apollo-angular";
+import {DxScrollViewModule, DxSelectBoxModule} from "devextreme-angular";
+import {Apollo, gql} from "apollo-angular";
+import DataSource from "devextreme/data/data_source";
+import ArrayStore from "devextreme/data/array_store";
 
 @Component({
   templateUrl: './application-monitoring.component.html',
@@ -9,11 +11,43 @@ import {Apollo} from "apollo-angular";
 })
 export class ApplicationMonitoringComponent {
 
-  monitoringUrl = '';
+  monitoringUrl = 'https://133.186.222.243:3000/grafana/d/JXj23h14k/jvm-micrometer?orgId=1&kiosk';
   applications;
 
   constructor(private apollo: Apollo) {
 
+    this.apollo.query({
+      query: gql`
+        query findAllApplications {
+          findAllApplications {
+            id
+            name
+            restPort
+            monitorPort
+            server {
+              name
+            }
+          }
+        }
+      `,
+      variables: {}
+    }).subscribe((result: any) => {
+      if (result.errors) {
+        console.error(result.errors);
+      }
+      console.log(result);
+      this.applications = new DataSource({
+        store: new ArrayStore({
+          data: result.data.findAllApplications,
+          key: 'id',
+        }),
+        group: 'server.name',
+      });
+    });
+  }
+
+  changeMonitoringUrl(e) {
+    console.log(e);
   }
 
 }
@@ -21,7 +55,8 @@ export class ApplicationMonitoringComponent {
 @NgModule({
   imports: [
     SafePipeModule,
-    DxSelectBoxModule
+    DxSelectBoxModule,
+    DxScrollViewModule
   ],
   providers: [],
   exports: [ApplicationMonitoringComponent],
