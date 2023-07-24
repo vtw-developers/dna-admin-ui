45f0ca7b6095e564be29c0ae3c25a0154ad0feb2
이승연 <seungyeon.lee@vtw.co.kr>
2023-07-24 17:22:05 +0900

    메뉴 설정페이지

---
+++ packages/angular/src/app/pages/role-menu/role-menu-inner.component.ts
@@ -0,0 +1,111 @@
+import {Component, Input, NgModule, ViewChild} from '@angular/core';
+import 'devextreme/data/odata/store';
+import {Apollo, gql} from "apollo-angular";
+import DataSource from "devextreme/data/data_source";
+import {
+  DxButtonModule,
+  DxDataGridComponent,
+  DxDataGridModule
+} from "devextreme-angular";
+import {CommonModule} from "@angular/common";
+import {PageableService} from "../../services/pageable.service";
+import {confirm} from 'devextreme/ui/dialog';
+import notify from "devextreme/ui/notify";
+import {Menu} from "../menu/menu.service";
+
+@Component({
+  selector: 'role-menu-inner',
+  templateUrl: './role-menu-inner.component.html',
+  providers: [PageableService],
+})
+export class RoleMenuInnerComponent {
+
+  @Input() set selectedCurrentItem(currentItem) {
+    this.currentItem = currentItem
+  };
+  menuType: DataSource;
+  selectedMenus: Number[] = [];
+  currentItem: any;
+  @ViewChild(DxDataGridComponent, {static: false}) grid: DxDataGridComponent;
+
+  constructor(private pageableService: PageableService, private apollo: Apollo) {
+    this.apollo.query({
+      query: gql`
+        query menuType($type: String) {
+          menuType(type: $type) {
+            id
+            name
+            detail
+            parentId
+            parentName
+            path
+            type
+            icon
+            expanded
+          }
+        }
+      `,
+      variables: {
+        type: 'Template'
+      }
+    }).subscribe({
+      next: (result: any) => {
+        this.menuType = result.data.menuType
+      },
+      error: (e) => {
+        console.error(e);
+        notify('예매 정보를 불러오는데 오류가 발생하였습니다.', 'error', 3000);
+      }
+    });
+  }
+
+  create(){
+    this.apollo.mutate({
+      mutation: gql`
+        mutation createRoleMenu($role: RolesInput, $menuList: [MenuInput]) {
+          createRoleMenu(role: $role, menuList: $menuList,) {
+            id
+          }
+        }
+      `,
+      variables: {
+        role: this.currentItem,
+        menuList: this.selectedMenus
+      }
+    }).subscribe({
+      next: (result: any) => {
+        console.log(result);
+        notify('메뉴가 설정되었습니다.', 'success', 3000);
+      },
+      error: (e) => {
+        console.error(e);
+        notify('메뉴 설정에 실패하였습니다.', 'error', 3000);
+      }
+    });
+  }
+
+  onSelectionChanged(e) {
+    this.selectedMenus = e.selectedRowsData;
+    console.log(this.selectedMenus);
+  }
+
+  refresh() {
+    this.menuType.reload();
+  }
+}
+
+@NgModule({
+  imports: [
+    DxButtonModule,
+    DxDataGridModule,
+
+    CommonModule,
+  ],
+  providers: [],
+  exports: [
+    RoleMenuInnerComponent
+  ],
+  declarations: [RoleMenuInnerComponent],
+})
+export class RoleMenuInnerModule {
+}
---
+++ packages/angular/src/app/pages/role-menu/role-menu.component.ts
@@ -0,0 +1,113 @@
+import {Component, NgModule, ViewChild} from '@angular/core';
+import 'devextreme/data/odata/store';
+import {Apollo, gql} from "apollo-angular";
+import {
+  DxButtonModule, DxContextMenuComponent, DxContextMenuModule,
+  DxDataGridComponent,
+  DxDataGridModule, DxScrollViewModule, DxTabPanelModule, DxTreeViewModule
+} from "devextreme-angular";
+import notify from "devextreme/ui/notify";
+import {CommonModule} from "@angular/common";
+import {
+  FlowTemplateNewFormModule,
+  TemplateNewFormComponent
+} from "../../components/library/dna/template-new-form/template-new-form.component";
+import {FormPopupComponent, FormPopupModule} from "../../components";
+import {
+  GroupNewFormComponent,
+  GroupNewFormModule
+} from "../../components/library/dna/group-new-form/group-new-form.component";
+import {TemplateDetailsModule} from "../dna/template-details/template-details.component";
+import {DxTreeViewComponent} from "devextreme-angular/ui/tree-view";
+import {RoleTab, UsersTwoModule} from "../role/role-tab.component";
+import {Role} from "../role/edit/role.service";
+import {RoleMenuInnerModule} from "./role-menu-inner.component";
+
+@Component({
+  templateUrl: './role-menu.component.html',
+  styleUrls: ['./role-menu.component.scss'],
+})
+export class RoleMenuComponent {
+
+  @ViewChild(GroupNewFormComponent) newGroupForm: GroupNewFormComponent;
+  @ViewChild(TemplateNewFormComponent) newTemplateForm: TemplateNewFormComponent;
+
+  @ViewChild(DxTreeViewComponent, {static: false}) treeView: DxTreeViewComponent;
+  @ViewChild(DxContextMenuComponent, {static: false}) contextMenu: DxContextMenuComponent;
+
+  @ViewChild(RoleTab, {static: false}) userRole: RoleTab;
+  @ViewChild('newDeptPopup', {static: false}) newDeptPopup: FormPopupComponent;
+  @ViewChild('newRolePopup', {static: false}) newRolePopup: FormPopupComponent;
+
+  treeItems: any[];
+  currentItem: Role;
+  selectedTreeItem: any;
+
+  constructor(private apollo: Apollo) {
+    this.apollo.query({
+      query: gql`
+        query rolesList($name: String) {
+          rolesList(name: $name) {
+            id
+            name
+            type
+            icon
+            expanded
+          }
+        }
+      `,
+      variables: {
+        name: ''
+      }
+    }).subscribe({
+      next: (result: any) => {
+        this.treeItems = result.data.rolesList;
+      },
+      error: (e) => {
+        console.error(e);
+        notify('오류가 발생하였습니다.', 'error', 3000);
+      }
+    });
+  }
+
+  selectItem(e) {
+    this.currentItem = {
+      id: e.itemData.id,
+      name: e.itemData.name,
+      detail: e.itemData.detail,
+      type: e.itemData.type,
+      icon: e.itemData.icon,
+      expanded: e.itemData.expanded
+    }
+  }
+
+  treeViewItemContextMenu(e) {
+    this.selectedTreeItem = e.itemData;
+  }
+
+  protected readonly parent = parent;
+}
+
+@NgModule({
+  imports: [
+    DxButtonModule,
+    DxDataGridModule,
+
+    CommonModule,
+    DxTreeViewModule,
+    DxTabPanelModule,
+    DxContextMenuModule,
+    DxScrollViewModule,
+    FlowTemplateNewFormModule,
+    FormPopupModule,
+    GroupNewFormModule,
+    TemplateDetailsModule,
+    UsersTwoModule,
+    RoleMenuInnerModule,
+  ],
+  providers: [],
+  exports: [],
+  declarations: [RoleMenuComponent],
+})
+export class RoleMenuModule {
+}
