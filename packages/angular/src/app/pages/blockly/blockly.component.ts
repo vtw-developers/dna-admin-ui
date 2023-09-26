50a9ddaff1439aba8b400acc82082323a10c916e
이승연 <seungyeon.lee@vtw.co.kr>
2023-09-26 11:33:36 +0900

    Blockly 기본 제공 블록 추가

--- packages/angular/src/app/pages/blockly/blockly.component.ts
+++ packages/angular/src/app/pages/blockly/blockly.component.ts
@@ -6,7 +6,6 @@ import {CommonModule} from "@angular/common";
 import Blockly from 'blockly';
 import {Block} from './blockly.service';
 import {javascriptGenerator} from 'blockly/javascript';
-import {ScrollOptions, ScrollBlockDragger, ScrollMetricsManager} from '@blockly/plugin-scroll-options';
 import {DxButtonModule, DxSelectBoxModule, DxTextBoxModule, DxValidatorModule} from "devextreme-angular";
 import {Apollo, gql} from "apollo-angular";
 import notify from "devextreme/ui/notify";
@@ -33,7 +32,7 @@ export class BlocklyComponent implements AfterViewInit {
   ngAfterViewInit(): void {
     this.workspace = Blockly.inject('blocklyDiv',
       {
-        toolbox: this.toolbox.nativeElement,
+        toolbox: this.initToolbox(),
         move: {
           scrollbars: {
             "horizontal": true,
@@ -92,7 +91,6 @@ export class BlocklyComponent implements AfterViewInit {
     if (e.ctrlKey && e.key === 's' || e.ctrlKey && e.key === 'S') {
       e.preventDefault();
       this.code = javascriptGenerator.workspaceToCode(this.workspace);
-
       const json = Blockly.serialization.workspaces.save(this.workspace);
       this.blockJson = JSON.stringify(json);
     }
@@ -105,7 +103,7 @@ export class BlocklyComponent implements AfterViewInit {
     this.block.blockJson = this.blockJson;
 
     if(this.finished == "완료"){
-     this.block.finished = true;
+      this.block.finished = true;
     } else if (this.finished == "미완료") {
       this.block.finished = false;
     }
@@ -131,7 +129,307 @@ export class BlocklyComponent implements AfterViewInit {
       }
     });
     this.router.navigate(['/blockly-list']);
-   }
+  }
+
+  initToolbox(): any {
+    let categoryToolbox = {
+      contents: undefined,
+      kind: undefined
+    };
+    categoryToolbox = {
+      kind: "categoryToolbox",
+      contents: [
+        {
+          "kind": "category",
+          "name": "Logic",
+          "colour": 210,
+          "contents": [
+            {
+              "kind": "block",
+              "type": "controls_if"
+            },
+            {
+              "kind": "block",
+              "type": "logic_compare",
+            },
+            {
+              "kind": "block",
+              "type": "logic_operation"
+            },
+            {
+              "kind": "block",
+              "type": "logic_negate",
+            },
+            {
+              "kind": "block",
+              "type": "logic_boolean"
+            },
+            {
+              "kind": "block",
+              "type": "logic_null",
+            },
+            {
+              "kind": "block",
+              "type": "logic_ternary"
+            },
+          ]
+        },
+        {
+          "kind": "category",
+          "name": "Loops",
+          "colour": 90,
+          "contents": [
+            {
+              "kind": "block",
+              "type": "controls_repeat_ext"
+            },
+            {
+              "kind": "block",
+              "type": "controls_repeat",
+            },
+            {
+              "kind": "BLOCK",
+              "type": "controls_whileUntil"
+            },
+            {
+              "kind": "BLOCK",
+              "type": "controls_for"
+            },
+            {
+              "kind": "BLOCK",
+              "type": "controls_forEach"
+            },
+            {
+              "kind": "BLOCK",
+              "type": "controls_flow_statements"
+            }
+          ]
+        },
+        {
+          "kind": "category",
+          "name": "Math",
+          "colour": 230,
+          "contents": [
+            {
+              "kind": "BLOCK",
+              "type": "math_number",
+              "gap": "32"
+            },
+            {
+              "kind": "BLOCK",
+              "type": "math_arithmetic"
+            },
+            {
+              "kind": "BLOCK",
+              "type": "math_single"
+            },
+            {
+              "kind": "BLOCK",
+              "type": "math_trig"
+            },
+            {
+              "kind": "BLOCK",
+              "type": "math_constant"
+            },
+            {
+              "kind": "BLOCK",
+              "type": "math_number_property"
+            },
+            {
+              "kind": "BLOCK",
+              "type": "math_round"
+            },
+            {
+              "kind": "BLOCK",
+              "type": "math_on_list"
+            },
+            {
+              "kind": "BLOCK",
+              "type": "math_modulo"
+            },
+            {
+              "kind": "BLOCK",
+              "type": "math_constrain"
+            },
+            {
+              "kind": "BLOCK",
+              "type": "math_random_int"
+            },
+            {
+              "kind": "BLOCK",
+              "type": "math_random_float"
+            },
+            {
+              "kind": "BLOCK",
+              "type": "math_atan2"
+            }
+          ]
+        },
+        {
+          "kind": "category",
+          "name": "Text",
+          "colour": 150,
+          "contents": [
+            {
+              "kind": "BLOCK",
+              "type": "text"
+            },
+            {
+              "kind": "BLOCK",
+              "type": "text_multiline"
+            },
+            {
+              "kind": "BLOCK",
+              "type": "text_join"
+            },
+            {
+              "kind": "BLOCK",
+              "type": "text_append"
+            },
+            {
+              "kind": "BLOCK",
+              "type": "text_length"
+            },
+            {
+              "kind": "BLOCK",
+              "type": "text_isEmpty"
+            },
+            {
+              "kind": "BLOCK",
+              "type": "text_indexOf"
+            },
+            {
+              "kind": "BLOCK",
+              "type": "text_charAt"
+            },
+            {
+              "kind": "BLOCK",
+              "type": "text_getSubstring"
+            },
+            {
+              "kind": "BLOCK",
+              "type": "text_changeCase"
+            },
+            {
+              "kind": "BLOCK",
+              "type": "text_trim"
+            },
+            {
+              "kind": "BLOCK",
+              "type": "text_count"
+            },
+            {
+              "kind": "BLOCK",
+              "type": "text_replace"
+            },
+            {
+              "kind": "BLOCK",
+              "type": "text_reverse"
+            },
+            {
+              "kind": "BLOCK",
+              "type": "text_print"
+            },
+            {
+              "kind": "BLOCK",
+              "type": "text_prompt_ext"
+            }
+          ]
+        },
+        {
+          "kind": "category",
+          "name": "Lists",
+          "colour": 250,
+          "contents": [
+            {
+              "kind": "BLOCK",
+              "type": "lists_create_with"
+            },
+            {
+              "kind": "BLOCK",
+              "type": "lists_create_with"
+            },
+            {
+              "kind": "BLOCK",
+              "type": "lists_repeat"
+            },
+            {
+              "kind": "BLOCK",
+              "type": "lists_length"
+            },
+            {
+              "kind": "BLOCK",
+              "type": "lists_isEmpty"
+            },
+            {
+              "kind": "BLOCK",
+              "type": "lists_indexOf"
+            },
+            {
+              "kind": "BLOCK",
+              "type": "lists_getIndex"
+            },
+            {
+              "kind": "BLOCK",
+              "type": "lists_setIndex"
+            },
+            {
+              "kind": "BLOCK",
+              "type": "lists_getSublist"
+            },
+            {
+              "kind": "BLOCK",
+              "type": "lists_split"
+            },
+            {
+              "kind": "BLOCK",
+              "type": "lists_sort"
+            },
+            {
+              "kind": "BLOCK",
+              "type": "lists_reverse"
+            }
+          ]
+        },
+        {
+          "kind": "category",
+          "name": "Colour",
+          "colour": 30,
+          "contents": [
+            {
+              "kind": "BLOCK",
+              "type": "colour_picker"
+            },
+            {
+              "kind": "BLOCK",
+              "type": "colour_random"
+            },
+            {
+              "kind": "BLOCK",
+              "type": "colour_rgb"
+            },
+            {
+              "kind": "BLOCK",
+              "type": "colour_blend"
+            }
+          ]
+        },
+        {
+          "kind": "category",
+          "name": "Variables",
+          "colour": 330,
+          "custom": "VARIABLE"
+        },
+        {
+          "kind": "category",
+          "name": "Functions",
+          "colour": 280,
+          "custom": "PROCEDURE"
+        }
+      ]
+    }
+    return categoryToolbox;
+  }
 }
 
 @NgModule({
